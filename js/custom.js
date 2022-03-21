a65e6419a43c6456b421901d78446cff7019796b
Sohel Rana <95288178+Sohel53588@users.noreply.github.com>
2022-03-21 09:39:18 -0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,43 @@
+$(function () {
+    // sticky Header part start
+    //  Stykey Header  
+    $(window).scroll(function () {
+        var scrolling = $(this).scrollTop();
+
+        if (scrolling > 200) {
+            $('.navbar').addClass('sticky_header');
+        } else {
+            $('.navbar').removeClass('sticky_header');
+        }
+    });
+    // sticky Header part end
+
+
+    // up to top part start
+
+    //==== Back-to-top button
+    $(window).on('scroll', function (event) {
+        if ($(this).scrollTop() > 600) {
+            $('.up_to_top').fadeIn(200)
+        } else {
+            $('.up_to_top').fadeOut(200)
+        }
+    });
+    //==== Animate the scroll to top
+    $('.up_to_top').on('click', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: 0,
+        }, 1000);
+    });
+    // up to top part end
+
+
+    // counter js start
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+    // counter js end
+})
